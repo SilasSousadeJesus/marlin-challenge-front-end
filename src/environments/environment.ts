ffdf6a94995f78b49ed1729801760fd535b3d5ad
silassousa <silassousadejesus@gmail.com>
2023-05-01 23:43:03 -0300

    feat(): implementando conexão com api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     baseUrl: 'https://jsonplaceholder.typicode.com/posts',
-    baseUrlApi: 'marlin-challenge-back-end.azurewebsites.net/api/post'
+
+    baseUrlApi: 'marlin-challenge-back-end.azurewebsites.net'
   };
