b1d4f9ec97a9817571eef6af961f915b24fc363b
silassousa <silassousadejesus@gmail.com>
2023-05-02 00:32:26 -0300

    refactor(): corrgindo conexões

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     baseUrl: 'https://jsonplaceholder.typicode.com/posts',
-    baseUrlApi: 'https://marlin-challenge-back-end.azurewebsites.net'
+    baseUrlApi: 'https://marlin-challenge-back-end.azurewebsites.net/api/post'
   };
 
 
